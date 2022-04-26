1dc148d96f1cee6192f339df4fda8188e1e257de
Jay <jiyoung26091212@gmail.com>
2022-04-26 11:52:28 +0100

    feat : print hello world

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
-import { request } from "express";
-import "./App.css";
+// import './App.css';
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 
-function App() {}
+const ENDPOINT = 'http://127.0.0.1:4001';
+
+function App() {
+	const [response, setResponse] = useState('');
+	console.log(response);
+
+	useEffect(() => {
+		const socket = socketIOClient(ENDPOINT);
+		socket.on('FromAPI', (data) => {
+			setResponse(data);
+		});
+	}, []);
+
+	return (
+		<p>
+			hello
+			<time dateTime={response}>{response}</time>
+		</p>
+	);
+}
 
 export default App;
